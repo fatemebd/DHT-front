1f6eaa4d5b741c46543d3de26bad2ae1d7da8003
fateme <fateme.s.e.81@gmail.com>
2024-08-18 21:29:34 +0330

    feat: init commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,13 +8,35 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        primary: {
+          100: "rgba(115, 57, 237,  0.1)",
+          200: "rgba(115, 57, 237,  0.2)",
+          300: "rgba(115, 57, 237,  0.3)",
+          400: "rgba(115, 57, 237,  0.4)",
+          500: "rgba(115, 57, 237,  0.5)",
+          600: "rgba(115, 57, 237,  0.6)",
+          700: "rgba(115, 57, 237,  0.7)",
+          800: "rgba(115, 57, 237,  0.8)",
+          900: "rgba(115, 57, 237,  0.9)",
+          1000: "rgba(115, 57, 237,  1)",
+        },
+        secondary: {
+          100: "rgba(4, 13, 44, 0.1)",
+          200: "rgba(4, 13, 44, 0.2)",
+          300: "rgba(4, 13, 44, 0.3)",
+          400: "rgba(4, 13, 44, 0.4)",
+          500: "rgba(4, 13, 44, 0.5)",
+          600: "rgba(4, 13, 44, 0.6)",
+          700: "rgba(4, 13, 44, 0.7)",
+          800: "rgba(4, 13, 44, 0.8)",
+          900: "rgba(4, 13, 44, 0.9)",
+          1000: "rgba(4, 13, 44, 1)",
+        },
       },
     },
   },
   plugins: [],
 };
+
 export default config;
