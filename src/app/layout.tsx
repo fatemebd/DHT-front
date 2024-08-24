78bf4983a51fb7b971da5a421c797ed46674e934
fateme <fateme.s.e.81@gmail.com>
2024-08-24 16:12:44 +0330

    fix: edit antd theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,23 +1,44 @@
-"use client"
+"use client";
+import { SessionProvider } from "next-auth/react";
 import "./globals.css";
-import  QueryClientProvider  from "@/providers/QueryClientProvider";
-
-
-
+import QueryClientProvider from "@/providers/QueryClientProvider";
+import { ConfigProvider } from "antd";
+import faLocale from "antd/lib/locale/fa_IR";
 
 export default function RootLayout({
   children,
+  session,
 }: Readonly<{
   children: React.ReactNode;
+  session: any;
 }>) {
   return (
-       <html>
-      
-        <body>    <QueryClientProvider> 
-{children}    </QueryClientProvider>
-</body>
-      </html>
-
-     
+    <html>
+      <body
+        dir="rtl"
+        className="h-screen flex justify-center items-center bg-gray-100"
+      >
+        {" "}
+        <ConfigProvider
+          locale={faLocale}
+          theme={{
+            components: {
+              Form: { labelColor: "#fff" },
+            },
+            token: {
+              // Seed Token
+              colorPrimary: "rgba(115, 57, 237,  1)",
+              fontFamily: "iranSans",
+              colorSplit: "#fff",
+              colorText: "#fff",
+            },
+          }}
+        >
+          <SessionProvider session={session}>
+            <QueryClientProvider>{children} </QueryClientProvider>
+          </SessionProvider>
+        </ConfigProvider>
+      </body>
+    </html>
   );
 }
