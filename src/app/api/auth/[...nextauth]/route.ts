4766ad62cdb4f272527dcbd3f887764f66b88558
fateme <fateme.s.e.81@gmail.com>
2024-08-25 18:45:12 +0330

    fix: edit the .env variables

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,22 +1,18 @@
-/** app/api/auth/[...nextauth]/route.ts */
-
 import NextAuth from "next-auth";
 import type { AuthOptions } from "next-auth";
-import { JWT } from "next-auth/jwt";
 import GithubProvider from "next-auth/providers/github";
 
 
 export const authOptions: AuthOptions = {
   providers: [
     GithubProvider({
-      clientId: process.env.GITHUB_ID as string,
-      clientSecret: process.env.GITHUB_SECRET as string,
+      clientId: process.env.NEXT_PUBLIC_GITHUB_ID as string,
+      clientSecret: process.env.NEXT_PUBLIC_GITHUB_SECRET as string,
     }),
   ],
   callbacks: {
     async jwt({ token, user }) {
-      console.log(user,"@@",token);
-      return token
+      return token;
     },
   },
 };
