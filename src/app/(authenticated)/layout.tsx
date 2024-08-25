68fd2749a5343fbe15e0e76057d29fb5b386750e
fateme <fateme.s.e.81@gmail.com>
2024-08-25 18:44:42 +0330

    feat: add react toastify and its config

--- src/app/(authenticated)/layout.tsx
+++ src/app/(authenticated)/layout.tsx
@@ -1,19 +1,27 @@
-"use client"
-import React, { ReactNode } from 'react'
-import {  useSession } from "next-auth/react";
-import { redirect } from "next/navigation";
+"use client";
 
-
+import React, { ReactNode } from "react";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
 interface LayoutProps {
   children: ReactNode;
 }
 
 const AuthenticatedLayout = ({ children }: LayoutProps) => {
-    const { data: session } = useSession();
-    if (session === null) {
-      redirect("/login");
-    }
+  const { data: session, status } = useSession();
+  const router = useRouter();
+
+  // Redirect to login if not authenticated
+  if (status === "loading") {
+    return <div>Loading...</div>;
+  }
+
+  if (status === "unauthenticated") {
+    router.push("/login");
+    return null; // Prevent further rendering
+  }
+
   return <main className="mx-3 mt-5 h-full w-full">{children}</main>;
 };
 
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,8 @@ import "./globals.css";
 import QueryClientProvider from "@/providers/QueryClientProvider";
 import { ConfigProvider } from "antd";
 import faLocale from "antd/lib/locale/fa_IR";
-
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 export default function RootLayout({
   children,
   session,
@@ -16,7 +17,7 @@ export default function RootLayout({
     <html>
       <body
         dir="rtl"
-        className="h-screen flex justify-center items-center bg-gray-100"
+        className="h-screen flex justify-center items-center bg-secondary-1000"
       >
         {" "}
         <ConfigProvider
@@ -25,9 +26,11 @@ export default function RootLayout({
             components: {
               Form: { labelColor: "#fff" },
             },
+            
             token: {
               // Seed Token
               colorPrimary: "rgba(115, 57, 237,  1)",
+              // colorBgBase:"rgba(4, 13, 44, 0.2)",
               fontFamily: "iranSans",
               colorSplit: "#fff",
               colorText: "#fff",
@@ -35,6 +38,7 @@ export default function RootLayout({
           }}
         >
           <SessionProvider session={session}>
+            <ToastContainer theme="dark"/>
             <QueryClientProvider>{children} </QueryClientProvider>
           </SessionProvider>
         </ConfigProvider>
