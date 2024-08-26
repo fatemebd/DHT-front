2c1f180fcad0af0621320f3afcd7a5480111c2ac
fateme <fateme.s.e.81@gmail.com>
2024-08-26 23:14:29 +0330

    feat: add base structure of homepage

--- src/app/(authenticated)/homepage/page.tsx
+++ src/app/(authenticated)/homepage/page.tsx
@@ -1,17 +1,50 @@
 "use client";
-import { Button, Form, Input, Modal } from "antd";
+import { Button, Col, Form, Input, Modal, Row } from "antd";
 import React, { useState } from "react";
 import { signOut } from "next-auth/react";
-
+import Header from "./components/Header";
+import { SearchOutlined } from "@ant-design/icons";
+import fa from "./fa.json"
 const Page = () => {
   const [signupModalOpen, setSignUpModalOpen] = useState(false);
   const [data, setData] = useState({});
   const [form] = Form.useForm();
+  const { Search } = Input;
 
+  const handleSearch = (value: string) => {
+    if (value) {
+      window.location.href = `https://www.google.com/search?q=${encodeURIComponent(
+        value
+      )}`;
+    }
+  };
   return (
-    <div>
-      <button onClick={() => signOut()}>Sign out</button>
+    <div className="px-40">
+      <Header />
+      <Row justify="space-between" className="  h-screen w-full">
+        <Col md={6} className="bg-white opacity-10 rounded-lg">
+          {/* to do list */}
+          to do list
+        </Col>
+        <Col md={10}>
+          <Search
+            placeholder={fa.googleSearch}
+            allowClear
+            enterButton={<SearchOutlined />}
+            size="large"
+            onSearch={handleSearch}
+          />
+        </Col>
+        <Col md={6} className="bg-white opacity-10 rounded-lg">
+          {/* mood tracker and habits*/}
+          mood tracker
+        </Col>
+      </Row>
     </div>
+
+    // <div>
+    //   <button onClick={() => signOut()}>Sign out</button>
+    // </div>
   );
 };
 
