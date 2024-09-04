3fe7ffdc1625381fd022698b5cd7980f64c9c1f1
fateme <fateme.s.e.81@gmail.com>
2024-09-04 16:33:22 +0330

    fis: integrate profile pic

--- src/app/(authenticated)/(startedWork)/dashboard/profile/api/index.ts
+++ src/app/(authenticated)/(startedWork)/dashboard/profile/api/index.ts
@@ -1,22 +1,49 @@
 import axiosInstance from "@/utils/axios";
-import { PUT_USER_PROFILE } from "./constants";
+import { PUT_USER_PROFILE, PUT_USER_PROFILE_PICTURE } from "./constants";
 import type { User } from "@/@types/common";
 import { useMutation } from "@tanstack/react-query";
 import { useGetUserDetail } from "@/app/(authenticated)/(startedWork)/api";
+import type { UploadFile } from "antd";
 
 const updateUser = async (user: Partial<User>) => {
-	const response = await axiosInstance.put(PUT_USER_PROFILE, user);
-	return response;
+  const response = await axiosInstance.put(PUT_USER_PROFILE, user);
+  return response;
+};
+
+const updateUserPic = async (file: File) => {
+  const formData: FormData = new FormData();
+  formData.append("picture", file);
+
+  const response = await axiosInstance.put(PUT_USER_PROFILE_PICTURE, formData, {
+    // headers: {
+    //   "Content-Type": undefined, // This should allow the browser to handle the content type automatically.
+    // },
+  });
+
+  return response;
 };
 
 export const useUpdateUser = () => {
-	const { refetch } = useGetUserDetail();
+  const { refetch } = useGetUserDetail();
 
-	return useMutation({
-		mutationKey: [PUT_USER_PROFILE],
-		mutationFn: updateUser,
-		onSuccess: () => {
-			refetch();
-		},
-	});
+  return useMutation({
+    mutationKey: [PUT_USER_PROFILE],
+    mutationFn: updateUser,
+    onSuccess: () => {
+      refetch();
+    },
+  });
+};
+
+export const useUpdateUserPic = () => {
+  const { refetch } = useGetUserDetail();
+
+  return useMutation({
+    mutationKey: [PUT_USER_PROFILE_PICTURE],
+    mutationFn: updateUserPic,
+    onError: (e) => console.error(e),
+    onSuccess: () => {
+      refetch();
+    },
+  });
 };
--- src/app/(authenticated)/(startedWork)/dashboard/profile/page.tsx
+++ src/app/(authenticated)/(startedWork)/dashboard/profile/page.tsx
@@ -9,7 +9,9 @@ import {
   type DatePickerProps,
   Avatar,
   Typography,
+  message,
   Spin,
+  Modal,
 } from "antd";
 import { UploadOutlined, UserOutlined } from "@ant-design/icons";
 import {
@@ -18,26 +20,38 @@ import {
 } from "antd-jalali";
 import { convertJalaaliToGregorian } from "@/utils/dateUtils";
 import fa from "./fa.json";
-import { useUpdateUser } from "./api";
+import { useUpdateUser, useUpdateUserPic } from "./api";
 import { toast } from "react-toastify";
 import type { User } from "@/@types/common";
 import { AxiosError } from "axios";
 import globalFa from "@/fa.json";
 import { useGetUserDetail } from "../../api";
+import { MdEdit } from "react-icons/md";
+const { Dragger } = Upload;
+import type { UploadFile, UploadProps } from "antd/lib/upload/interface";
+import { IoCloseOutline } from "react-icons/io5";
 
 const Page = () => {
+  useJalaliLocaleListener();
+
   const [form] = Form.useForm();
+
   const [birthDate, setBirthDate] = useState("2024-9-1");
+  const [profileModal, setProfileModal] = useState(false);
+
   const { data: user } = useGetUserDetail();
+
+  const [fileList, setFileList] = useState<UploadFile[]>([]);
 
   const { mutate: updateUserMutate, isPending: isUserUpdatePending } =
     useUpdateUser();
 
+  const { mutate: updateUserPicMutate, isPending: isUserPicUpdatePending } =
+    useUpdateUserPic();
+
   const onChange: DatePickerProps["onChange"] = (date) => {
     setBirthDate(convertJalaaliToGregorian(date.format("YYYY-MM-DD")));
   };
-
-  useJalaliLocaleListener();
 
   useEffect(() => {
     form.setFieldsValue(user);
@@ -48,10 +62,24 @@ const Page = () => {
     );
   }, [user, form]);
 
+  const handleOpenModal = () => {
+    setProfileModal(true);
+  };
+
+  const handleCloseModal = () => {
+    onRemovePic();
+    setProfileModal(false);
+  };
+
   const handleActionFailed = (error: unknown) => {
     if (error instanceof AxiosError) {
       toast.error(error.message);
     }
+  };
+
+  const handleActionSucceeded = () => {
+    toast.success(globalFa.updatedSuccessfully);
+    handleCloseModal();
   };
 
   const onFinish = (values: Partial<User>) => {
@@ -60,83 +88,119 @@ const Page = () => {
       dateOfBirth: birthDate,
     };
     updateUserMutate(data, {
-      onSuccess: () => toast.success(globalFa.updatedSuccessfully),
+      onSuccess: handleActionSucceeded,
       onError: handleActionFailed,
     });
   };
 
+  const beforeUpload: UploadProps["beforeUpload"] = (file) => {
+    if (fileList.length >= 1) {
+      message.error("You can only upload one file");
+      return false;
+    }
+    setFileList([file]);
+    return false; // Prevent auto upload
+  };
+
+  const onRemovePic = () => {
+    setFileList([]);
+  };
+
+  const handleChangeProfile = () => {
+    console.log("fvfv");
+    
+    if (fileList[0]) {
+      // biome-ignore lint/suspicious/noExplicitAny: <explanation>
+      updateUserPicMutate(fileList[0] as any as File, {
+        onError: handleActionFailed,
+        onSuccess: handleActionSucceeded,
+      });
+    }
+  };
+
   return (
-    <Form
-      form={form}
-      onFinish={onFinish}
-      layout="vertical"
-      className="md:px-72 px-5"
-    >
-      <div className="mb-8 flex items-center gap-5">
-        <Avatar
-          src={user?.picture}
-          icon={user?.picture ? "" : <UserOutlined />}
-          size={64}
-          className="mb-2"
-        />
-        <div>
-          <Typography>{user?.email}</Typography>
-          <Typography>
-            {user?.firstName} {user?.lastName}
-          </Typography>
-        </div>
-      </div>
-      <Form.Item name="firstName" label={fa.firstName}>
-        <Input />
-      </Form.Item>
-      <Form.Item name="lastName" label={fa.lastName}>
-        <Input />
-      </Form.Item>
-
-      <Form.Item name="dateOfBirth" label={fa.birthDate}>
-        <DatePickerJalali onChange={onChange} className="text-black w-full" />
-      </Form.Item>
-
-      <Form.Item
-        name="avatar"
-        valuePropName="fileList"
-        // biome-ignore lint/suspicious/noExplicitAny: <explanation>
-        getValueFromEvent={(e: any) => {
-          if (Array.isArray(e)) {
-            return e;
-          }
-          return e?.fileList;
+    <>
+      <Modal
+        closeIcon={<IoCloseOutline className="text-white " />}
+        title={fa.uploadProfile}
+        open={profileModal}
+        onCancel={handleCloseModal}
+        onClose={handleCloseModal}
+        okButtonProps={{
+          icon: isUserPicUpdatePending && <Spin />,
+          disabled:isUserPicUpdatePending
         }}
+        onOk={handleChangeProfile}
       >
-        <Upload
-          disabled={!user?.allowedChangeProfile}
-          className="rounded-full"
-          name="avatar"
-          listType="picture-card"
-          showUploadList={false}
-          //   customRequest={dummyRequest}
+        <Dragger
+          accept=".png,.jpg,.jpeg"
+          beforeUpload={beforeUpload}
+          fileList={fileList}
+          onRemove={onRemovePic}
+          multiple={false}
+          showUploadList={{
+            showRemoveIcon: true,
+          }}
+          className=" hover:text-primary-1000 duration-[0.3s]"
         >
-          {!form.getFieldValue("avatar") ||
-          form.getFieldValue("avatar").length === 0 ? (
-            <div>
-              <UploadOutlined />
-              <div style={{ marginTop: 0 }}>{fa.uploadProfile}</div>
-            </div>
-          ) : null}
-        </Upload>
-      </Form.Item>
+          <div className="flex flex-col gap-3 items-center my-5 ">
+            <UploadOutlined />
+            {fa.dragDrop}
+          </div>
+        </Dragger>
+      </Modal>
+      <Form
+        form={form}
+        onFinish={onFinish}
+        layout="vertical"
+        className="md:px-72 px-5"
+      >
+        <div className="mb-8 flex items-center gap-5">
+          <div className="w-fit relative">
+            <Avatar
+              src={user?.picture}
+              icon={user?.picture ? "" : <UserOutlined />}
+              size={72}
+              className="mb-2"
+            />
+            <Button
+              icon={<MdEdit className="text-white text-sm" />}
+              type="primary"
+              // onClick={handleOpenModal}
+              className="absolute top-14 left-1 z-50 w-6 h-6 rounded-full p-0"
+              disabled={!user?.allowedChangeProfile}
+              onClick={user?.allowedChangeProfile ? handleOpenModal : undefined}
+            />
+          </div>
+          <div>
+            <Typography>{user?.email}</Typography>
+            <Typography>
+              {user?.firstName} {user?.lastName}
+            </Typography>
+          </div>
+        </div>
+        <Form.Item name="firstName" label={fa.firstName}>
+          <Input />
+        </Form.Item>
+        <Form.Item name="lastName" label={fa.lastName}>
+          <Input />
+        </Form.Item>
 
-      <Form.Item>
-        <Button
-          type="primary"
-          htmlType="submit"
-          disabled={isUserUpdatePending}
-          icon={isUserUpdatePending && <Spin />}
-        >
-          {fa.edit}
-        </Button>
-      </Form.Item>
-    </Form>
+        <Form.Item name="dateOfBirth" label={fa.birthDate}>
+          <DatePickerJalali onChange={onChange} className="text-black w-full" />
+        </Form.Item>
+        <Form.Item>
+          <Button
+            type="primary"
+            htmlType="submit"
+            disabled={isUserUpdatePending}
+            icon={isUserUpdatePending && <Spin />}
+          >
+            {fa.edit}
+          </Button>
+        </Form.Item>
+      </Form>
+    </>
   );
 };
 
