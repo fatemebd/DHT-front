3bb5f7d81db1e332ffdeb75177c922dcb72e3c18
fateme <fateme.s.e.81@gmail.com>
2024-09-04 21:36:24 +0330

    fix: fix some issues

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -16,18 +16,18 @@ firebase.initializeApp(firebaseConfig);
 // eslint-disable-next-line no-undef
 const messaging = firebase.messaging();
 
-messaging.onBackgroundMessage((payload) => {
-  self.registration.showNotification(payload.message.data.title, {
-    body: payload.message.data.body,
-    icon: "./logo.png",
-  });
+// messaging.onBackgroundMessage((payload) => {
+//   self.registration.showNotification(payload.data.title, {
+//     body: payload.data.body,
+//     icon: "./logo.png",
+//   });
 
-  // biome-ignore lint/nursery/noConsole: <explanation>
-  console.log(
-    "[firebase-messaging-sw.js] Received background message ",
-    payload
-  );
-});
+//   // biome-ignore lint/nursery/noConsole: <explanation>
+//   console.log(
+//     "[firebase-messaging-sw.js] Received background message ",
+//     payload
+//   );
+// });
 self.addEventListener("notificationclick", (event) => {
   // Example: Open a specific URL when the notification is clicked
   event.waitUntil(
@@ -35,10 +35,17 @@ self.addEventListener("notificationclick", (event) => {
       // Send data back to the main thread using postMessage
       event.notification.close(); // Close the notification
 
+      const promise = new Promise((resolve) => {
+        setTimeout(resolve, 3000);
+      }).then(() => {
+        // return the promise returned by openWindow, just in case.
+        // Opening any origin only works in Chrome 43+.
+        if (windowClient) {
+          return windowClient.focus(); // Focus the existing tab
+        }
+        return;
+      });
       // Check if the client (tab) exists
-      if (windowClient) {
-        windowClient.focus(); // Focus the existing tab
-      }
 
       // Send event data to the main thread for storage
       clients
